13154316d03974763dde68f819485f25a1c41d1d
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-01 02:01:32 -0700

    Test integration.

---
+++ test/index.js
@@ -0,0 +1,91 @@
+/*jshint browser:true */
+"use strict";
+
+var open = require("../event")
+var reduced = require("reducers/reduced")
+var reduce = require("reducers/reduce")
+var filter = require("reducers/filter")
+var into = require("reducers/into")
+
+function click(target) {
+  var event = document.createEvent("MouseEvents")
+  event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+                       false, false, false, false, 0, null)
+  target.dispatchEvent(event)
+}
+
+exports["test the dom events are reduced"] = function(assert) {
+  var element = document.createElement("div")
+  document.body.appendChild(element)
+
+
+  var clicks = open(element, "click")
+  var count = 0
+  var result = reduce(clicks, function(state, event) {
+    assert.equal(event.target, element, "event target is element")
+    count = count + 1
+  }, 0)
+
+  click(element)
+  click(element)
+
+  assert.equal(count, 2, "reducer was called twice")
+  document.body.removeChild(element)
+}
+
+exports["test listeners removal"] = function(assert) {
+  var element = document.createElement("div")
+  document.body.appendChild(element)
+
+
+  var clicks = open(element, "click")
+  var count = 0
+  var result = reduce(clicks, function(state, event) {
+    count = count + 1
+    assert.equal(event.target, element, "event target is element")
+    return reduced("stop!")
+  }, 0)
+
+  click(element)
+  click(element)
+  click(element)
+
+  assert.equal(count, 1, "reducer stopped reduction early")
+  assert.deepEqual(into(result), ["stop!"], "reducer accumulates value")
+  document.body.removeChild(element)
+}
+
+
+exports["test multiple reducers"] = function(assert) {
+  var element = document.createElement("div")
+  document.body.appendChild(element)
+
+
+  var clicks = open(element, "click")
+  var profile = []
+
+  var r1 = reduce(clicks, function(state, event) {
+    profile.push(1)
+    assert.equal(event.target, element, "event target is element #1")
+    if (profile.length === 3) return reduced("stop!")
+  }, 0)
+
+  var r2 = reduce(clicks, function(state, event) {
+    profile.push(2)
+    assert.equal(event.target, element, "event target is element #2")
+  })
+
+  click(element)
+  click(element)
+  click(element)
+  click(element)
+  click(element)
+  click(element)
+
+  assert.deepEqual(profile, [1, 2, 1, 2, 2, 2, 2, 2],
+                   "events were delivered in expected order")
+  assert.deepEqual(into(r1), ["stop!"], "first reduce was stopped early")
+  document.body.removeChild(element)
+}
+
+require("test").run(exports)
